2da82c33810897656998ef0321717e33bb685fc5
yakovk <kor4ik@gmail.com>
2023-04-03 17:13:31 +0300

    Component SignIn Added: local storage rules: required, email, unique

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -5,10 +5,10 @@
     </h1>
     <v-card class="mx-auto px-6 py-8" max-width="344">
       <v-form v-model="form" @submit.prevent="onSubmit" padding>
-        <v-text-field v-model="email" :readonly="loading" :rules="[required]" class="mb-2" clearable
+        <v-text-field v-model="email" :readonly="loading" :rules="[requiredRule, emailRule, uniqueRule]" class="mb-2" clearable
           label="Email" variant="outlined">
         </v-text-field>
-        <v-text-field v-model="password" :readonly="loading" :rules="[required]" clearable label="Password" variant="outlined"
+        <v-text-field v-model="password" type="password" :readonly="loading" :rules="[requiredRule]" clearable label="Password" variant="outlined"
           placeholder="Enter your password">
         </v-text-field>
         <br>
@@ -21,8 +21,10 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'SignIp',
+  name: 'SignIn',
   data: () => ({
     form: false,
     email: null,
@@ -30,13 +32,34 @@ export default {
     loading: false
   }),
   methods: {
-    onSubmit () {
+    async onSubmit () {
       if (!this.form) return
       this.loading = true
-      setTimeout(() => (this.loading = false), 2000)
+      const result = await axios.post('http://localhost:3000/users', {
+        email: this.email,
+        password: this.password
+      })
+        .catch((err) => alert(err))
+      if (result.status === 201) {
+        localStorage.setItem('user-data', JSON.stringify(result.data))
+        this.loading = false
+        // console.log('User:', this.email, 'createed with data', result)
+      }
     },
-    required (v) {
+    requiredRule (v) {
       return !!v || 'Field is required'
+    },
+    emailRule (v) {
+      if (/^\w+([\\.-]?\w+)*@\w+([\\.-]?\w+)*(\.\w{2,3})+$/.test(v)) return true
+      return 'E-mail must be valid'
+    },
+    async uniqueRule (v) {
+      const result = await axios.get('http://localhost:3000/users', {
+        params: {
+          email: v
+        }
+      })
+      return !result.data.length || 'E-mail already exists'
     }
   }
 }
